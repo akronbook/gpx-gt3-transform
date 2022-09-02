360301f769b994b04e78c4530af350ace9cde79f
Yuanming Chen <yuanming@hotmail.com>
2022-09-01 22:20:29 -0600

    Create a simple console application

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GPX_GT3_Transform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputGpxFile = args[0];
+            var outputGpxFile = args[1];
+            Console.WriteLine($"Input File={inputGpxFile}, Output File={outputGpxFile}");
+        }
+    }
+}
