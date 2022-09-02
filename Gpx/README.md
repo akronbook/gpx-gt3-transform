647f957965b3d2c69ff84653862fbea13605997f
Yuanming Chen <yuanming@hotmail.com>
2022-09-01 22:44:50 -0600

    Use parser at https://github.com/macias/Gpx

---
+++ Gpx/README.md
@@ -0,0 +1,8 @@
+# Gpx
+
+C# Gpx Reader/Writer copied from http://dlg.krakow.pl/gpx/ with few extensions.
+
+
+## Building
+
+Please use Visual Studio 2017 or compatible tool.
--- Program.cs
+++ Program.cs
@@ -1,12 +1,52 @@
-﻿namespace GPX_GT3_Transform
+﻿using Gpx;
+using Gpx.Implementation;
+using Gpx.Tests;
+
+namespace GPX_GT3_Transform
 {
     class Program
     {
+        private static GpxTrack readTrack<TTrackPoint>(string path, IGpxTrackPointReader<TTrackPoint> trackPointReader = null)
+            where TTrackPoint : GpxTrackPoint, new()
+        {
+            GpxTrack track = null;
+
+            using (GpxIOFactory.CreateReader(path, trackPointReader, out IGpxReader reader, out _))
+            {
+                while (reader.Read(out GpxObjectType type))
+                {
+                    switch (type)
+                    {
+                        case GpxObjectType.Metadata:
+                            break;
+                        case GpxObjectType.WayPoint:
+                            break;
+                        case GpxObjectType.Route:
+                            break;
+                        case GpxObjectType.Track:
+                            {
+                                if (track == null)
+                                    track = reader.Track;
+                                else
+                                    throw new InvalidOperationException("Track is already read");
+                                break;
+                            }
+                    }
+                }
+            }
+
+            if (track == null)
+                throw new NullReferenceException("Track was not read");
+
+            return track;
+
+        }
+
         static void Main(string[] args)
         {
             var inputGpxFile = args[0];
             var outputGpxFile = args[1];
-            Console.WriteLine($"Input File={inputGpxFile}, Output File={outputGpxFile}");
+            GpxTrack track = readTrack(inputGpxFile, new ProximityTrackPointReader());
         }
     }
 }
